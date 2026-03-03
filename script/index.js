6852887c1be2f67f4a4cbe048b017e3de38462ee
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-03-03 21:50:46 +0600

    Let's Learn Section Added

--- script/index.js
+++ script/index.js
@@ -4,6 +4,29 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data));
 };
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data));
+    
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById('word-container');
+    // wordContainer.innerHTML = "";
+
+    words.forEach((word) => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <p> Cat </p>
+        `;
+        
+        wordContainer.append(card);
+        
+    });
+}
+
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
@@ -11,7 +34,7 @@ const displayLesson = (lessons) => {
   for (const lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-        <button href="" class="btn btn-outline btn-primary"><i   class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+        <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i   class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
         </button>
         `;
 
