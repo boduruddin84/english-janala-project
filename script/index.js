a5860e5274d21822cbf99e316c4601a252a55c66
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-03-04 22:35:25 +0600

    ShowModal Added

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
+const createElements = (arr) => {
+  const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+  return htmlElements.join(" ");
+}
+
 const loadLessons = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
@@ -8,7 +13,6 @@ const removeActive = () => {
   const lessonButton = document.querySelectorAll(".lesson-btn");
   lessonButton.forEach((btn) => btn.classList.remove("active"))
 };
-
 
 const loadLevelWord = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
@@ -21,6 +25,36 @@ const loadLevelWord = (id) => {
       displayLevelWord(data.data);
     });
 };
+
+const loadWordDetail = async(id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+}
+
+const displayWordDetails = (word) => {
+  const detailsBox = document.getElementById('details-container');
+  detailsBox.innerHTML = `
+          <div>
+            <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>:${word.pronunciation})</h2>
+          </div>
+          <div>
+            <h2 class="font-bold">Meaning</h2>
+            <p>${word.meaning}</p>
+          </div>
+          <div>
+            <h2 class="font-bold">Example</h2>
+            <p>${word.sentence}</p>
+          </div>
+          <div>
+            <h2 class="font-bold font-bangla">সমার্থক শব্দ গুলো</h2>
+            <div>${createElements(word.synonyms)}</div>
+          </div
+  
+  `;
+  document.getElementById('word_modal').showModal();
+}
 
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
@@ -47,7 +81,7 @@ const displayLevelWord = (words) => {
         <p class="font-semibold">Meaning /Pronunciation</p>
         <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
         <div class="flex justify-between items-center">
-          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
