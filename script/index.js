a233ee9f351381a69a52afd1c260645ad0806fb0
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-03-04 10:26:03 +0600

    Lesson Added

--- script/index.js
+++ script/index.js
@@ -5,27 +5,46 @@ const loadLessons = () => {
 };
 
 const loadLevelWord = (id) => {
-    const url = `https://openapi.programming-hero.com/api/level/${id}`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayLevelWord(data.data));
-    
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayLevelWord(data.data));
 };
 
 const displayLevelWord = (words) => {
-    const wordContainer = document.getElementById('word-container');
-    // wordContainer.innerHTML = "";
+  const wordContainer = document.getElementById("word-container");
+  wordContainer.innerHTML = "";
 
-    words.forEach((word) => {
-        const card = document.createElement("div");
-        card.innerHTML = `
-        <p> Cat </p>
+  
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center bg-sky-100 col-span-full rounded py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+    `;
+    return;
+  }
+
+  words.forEach((word) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+        <h2 class="font-bold text-2xl">${word.word}</h2>
+        <p class="font-semibold">Meaning /Pronunciation</p>
+        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+        <div class="flex justify-between items-center">
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+          <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+      </div>
         `;
-        
-        wordContainer.append(card);
-        
-    });
-}
+
+    wordContainer.append(card);
+  });
+};
 
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
